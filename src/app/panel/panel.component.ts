ab1107aa1e859d4e68bdfcb1ef09c6ff9a3b1c15
QuackCode <quack.code.dk@gmail.com>
2017-12-07 00:52:54 +0100

    panel.component.ts: fix removed declarations field (2)

--- src/app/panel/panel.component.ts
+++ src/app/panel/panel.component.ts
@@ -1,9 +1,15 @@
 import { Component, OnInit, NgModule } from '@angular/core';
-
+import {FormsModule} from '@angular/forms';
 @Component({
   selector: 'app-panel',
   templateUrl: './panel.component.html',
   styleUrls: ['./panel.component.css']
+})
+
+@NgModule({
+  imports:      [FormsModule],
+  declarations: [
+  ]
 })
 
 export class PanelComponent implements OnInit {
@@ -14,11 +20,3 @@ export class PanelComponent implements OnInit {
   }
 
 }
-
-@NgModule({
-  imports:      [FormsModule],
-  declarations: [
-    SignupComponent,
-    LoginComponent
-  ]
-})
