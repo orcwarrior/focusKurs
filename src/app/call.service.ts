8ff23792709c9049fba22b099edccc1768c621e1
QuackCode <quack.code.dk@gmail.com>
2017-12-07 17:32:45 +0100

    ng: working router (npm update needed for new rxjs)

--- src/app/call.service.ts
+++ src/app/call.service.ts
@@ -1,23 +1,43 @@
-import { Injectable } from '@angular/core'
+import {Injectable} from '@angular/core'
 import {Http, Response, RequestOptions, Headers} from "@angular/http"
 import {post} from "selenium-webdriver/http"
 
 @Injectable()
 export class CallService {
   private apiUrl = 'http://localhost:3000'
+  private callStatus = null
+  _generateRequestOptions = function (additionalOptions = {}) {
+    const headers = new Headers({'Content-Type': 'application/json'})
+    let options = new RequestOptions({headers: headers})
+    options = Object.assign(options, additionalOptions);
+    return options;
+  }
 
-  constructor(private http: Http) { }
+  constructor(private http: Http) {
+  }
 
   placeCall(number: string) {
-    let postData = JSON.stringify({ first_number: '500127424', second_number: number })
-    let options = new RequestOptions({ headers: new Headers({ 'Content-Type': 'application/json' }) })
+    const postData = JSON.stringify({first_number: '500127424', second_number: number})
+    const options = this._generateRequestOptions();
     // let headers ...
     this.http.post(this.apiUrl + "/call", postData, options)
       .toPromise() // obietnica / Promise
-      .then(function(response) {
-          console.log('Response received:')
-          console.log(response);
+      .then(function (response) {
+        console.log('Response received:')
+        console.log(response);
       });
   }
+
+  // checkStatus(callId: number) {
+  //   const options = this._generateRequestOptions({search: {id: callId}})
+//
+  //   this.http.get(this.apiUrl + '/status', options)
+  //     .toPromise()
+  //     .then((response) => {
+  //       // response = JSON.parse(response); // parse response string to JSON object
+  //       this.callStatus.next(response.status);
+  //     })
+  // }
+
 
 }
