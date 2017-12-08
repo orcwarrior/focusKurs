0724fa183c5c09a8b9e72d56c4e40c45f7e15b18
QuackCode <quack.code.dk@gmail.com>
2017-12-08 18:09:12 +0100

    Ringing.component: now it's responsible for placing call based on route param url (:numer)

--- src/app/ringing/ringing.component.ts
+++ src/app/ringing/ringing.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import {CallService} from "../call.service";
 
 @Component({
   selector: 'app-ringing',
@@ -6,10 +8,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./ringing.component.css']
 })
 export class RingingComponent implements OnInit {
+  private numer: number;
+  private status: string;
+  constructor(private route: ActivatedRoute, private callService: CallService) {
 
-  constructor() { }
+  }
+  updateCallStatus() {
 
+    this.status = "Nowy status pobrany z serwera na 3000...";
+  }
   ngOnInit() {
+    // Należy zachować referencje do this, jako że w wywołaniu funkcji
+    // assignNumerFromParams kontekst this ulegnie zmianie i nie będzie wskazywało
+    // na nasz komponent do którego chcemy przypisać pole numer.
+    let self = this;
+    this.route.params.subscribe(function assignNumerFromParams(params) {
+      self.numer = params.numer;
+      self.callService.placeCall(self.numer);
+    })
   }
 
 }
