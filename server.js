ee029bbc01acb070eccce7767ba05f37608218f1
QuackCode <quack.code.dk@gmail.com>
2017-12-09 03:07:10 +0100

    server.js: (expand, long message) * Make status/:number service fully working, added app.param use, make bridges map from one single bridge object so multiple calls can be registred at once. * Call service is now catching dialer lib exceptions and if so the error is passed as response and properly handled by status service.

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const {Connector, Dialer} = require('dialer');
 const cors = require('cors');
-const bodyParser = require('body-parser')
+const bodyParser = require('body-parser');
 
 let app = express();
 let c = {
@@ -10,29 +10,55 @@ let c = {
 };
 let dialer = new Dialer(new Connector(c.url, c.login, c.password));
 
-// app.use(express.bodyParser());
-app.use(cors());
-app.use(bodyParser.json());
 
 app.listen(3000, function () {
   console.log('Utworzono serwis dla focusKurs na porcie 3000!')
 });
 
-var bridge = null;
-app.post('/call',function (req, res) {
-  console.log('Call service called!')
-  console.log(req.body)
+// Generic middlewares:
+app.use(cors());
+app.use(bodyParser.json());
+
+// Call bridges to teraz obiekt słownikowy, gdzie kluczem (nazwą pola) bedzie numer
+// do którego było wydzwanianie, a vartoscia bedzie obiekt bridge z zapytania.
+// TODO: Usuwanie z mapy bridge'ów połączeń które zostały juz zakończone.
+let callBridges = {};
+
+app.post('/call', async function (req, res) {
+  console.log('Call service called!');
+  console.log(req.body);
   console.log('Nawiazywanie polaczenia miedzy numerami: ' + req.body.first_number + ', ' + req.body.second_number + '...');
-  dialer.call(req.body.first_number, req.body.second_number)
-    .then(function(_bridge) {
-      bridge = _bridge;
+  try {
+    let bridge = await dialer.call(req.body.first_number, req.body.second_number);
+    callBridges[req.body.first_number] = bridge;
+    console.log('bridges:', callBridges);
+  } catch (dialerError) {
+    console.error(dialerError.stack);
+    return res.json({
+      status: 'ERROR',
+      msg: 'Nawiazywanie połaczenia nieudane z powodu wystąpienia błędu',
+      err: dialerError.stack
     })
-  res.json({status: 'initiated', msg: 'Nawiazywanie polaczenia miedzy numerami: ' + req.body.first_number + ', ' + req.body.second_number + '...'})
+  }
+  res.json({
+    status: 'SUCCESS',
+    msg: 'Nawiazywanie polaczenia miedzy numerami: ' + req.body.first_number + ', ' + req.body.second_number + '...'
+  })
 });
 
-app.get('/status',async function (req, res) {
-  let id = req.query.id;
-  let status = await bridge.getStatus()
-  res.json({success: true, status: status})
-  console.log('Zapytanie o status: ' + status)
+
+// Middleware obsługujacy parametr numer dla
+app.param('numer', function (req, res, next, numer) {
+  req.numer = numer;
+  next();
+});
+app.get('/status/:numer', async function (req, res) {
+  // console.log(callBridges);
+  if (!callBridges[req.numer])
+    return res.json({success: false, status: 'NODE: CALL NOT FOUND'});
+  else {
+    let status = await bridge.getStatus();
+    res.json({success: true, status: status});
+    console.log('Zapytanie o status: ' + status)
+  }
 });
