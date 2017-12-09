b197cebf6842be9e3b2ef712254e58a24f5182b9
QuackCode <quack.code.dk@gmail.com>
2017-12-09 04:31:13 +0100

    call.service: Lot of refactoring done

--- src/app/call.service.ts
+++ src/app/call.service.ts
@@ -1,48 +1,48 @@
-import {Injectable} from '@angular/core'
-import {Http, Response, RequestOptions, Headers} from "@angular/http"
-import {post} from "selenium-webdriver/http"
+import {Injectable} from '@angular/core';
+import {Http, Response, RequestOptions, Headers} from '@angular/http';
 
 @Injectable()
 export class CallService {
-  private apiUrl = 'http://localhost:3000'
-  private callStatus = null
-  _generateRequestOptions = function (additionalOptions = {}) {
-    const headers = new Headers({'Content-Type': 'application/json'})
-    let options = new RequestOptions({headers: headers})
-    options = Object.assign(options, additionalOptions);
-    return options;
-  }
+  private apiBaseUrl = 'http://localhost:3000';
+  private apiServicesUrls = {
+    call: this.apiBaseUrl + '/call',
+    status: (phoneNumber) => this.apiBaseUrl + `/status/${phoneNumber}`
+  };
 
-  _parseResponseBody(response) {
-    let parsedResponse = Object.assign({}, response);
-    parsedResponse.body = JSON.parse(response._body); // _body -> body zamierzone
-    return parsedResponse;
-  }
+  private callStatus = null;
 
   constructor(private http: Http) {
   }
 
-  // Metoda place call od teraz zwraca obietnice (Promise)
-  // stat składnia nazwa_funkcji(parametry) : typ_zwracanej_wartosci
   placeCall(number: string): Promise<any> {
-    const postData = JSON.stringify({first_number: '500127424', second_number: number})
+    const postData = JSON.stringify({first_number: '500127424', second_number: number});
     const options = this._generateRequestOptions();
 
-    return this.http.post(this.apiUrl + "/call", postData, options)
-      .toPromise() // obietnica / Promise
-      .then(function (response) {
-        return response;
-      });
+    return this.http.post(this.apiServicesUrls.call, postData, options)
+      .toPromise()
+      .then(this._parseResponseBody);
   }
 
   checkStatus(numer: string): Promise<any> {
     const options = this._generateRequestOptions();
-    let self = this;
-    return this.http.get(this.apiUrl + '/status/' + numer, options)
+    const self = this;
+    return this.http.get(this.apiServicesUrls.status(numer), options)
       .toPromise()
-      .then(function (response) {
-        return self._parseResponseBody(response);
-      })
+      .then(this._parseResponseBody);
+  }
+
+  /* private methods*/
+  private _generateRequestOptions = function (additionalOptions: Object = {}) {
+    const headers = new Headers({'Content-Type': 'application/json'});
+    let options = new RequestOptions({headers: headers});
+    options = Object.assign(options, additionalOptions);
+    return options;
+  };
+
+  private _parseResponseBody(response) {
+    const parsedResponse = Object.assign({}, response);
+    parsedResponse.body = JSON.parse(response._body); // _body -> body zamierzone
+    return parsedResponse;
   }
 
 
