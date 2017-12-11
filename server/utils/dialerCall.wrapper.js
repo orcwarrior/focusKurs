544e0714f1ba9f619a0addb7384cd01471ed5d20
QuackCode <quack.code.dk@gmail.com>
2017-12-11 04:31:32 +0100

    dialerCall.wrapper: fixed Bridge (using smartBridge method which actually calls .bridge() to bridge 2 calls)

--- server/utils/dialerCall.wrapper.js
+++ server/utils/dialerCall.wrapper.js
@@ -5,20 +5,42 @@ import {getDialerIdByPhoneNumbers} from './dialerCall.store'
 const connector = new Connector(apiCredentials.url, apiCredentials.login, apiCredentials.password)
 const callFactory = new CallFactory(connector);
 
+// TODO: Move to smartBridge
+function timeout(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+async function waitUntilIsNotPending(bridge) {
+  do {
+    var bridgeStatus = await bridge.getStatus();
+    // debug('check status', bridgeStatus);
+    if(bridgeStatus === bridge.STATUSES.PENDING) {
+      await timeout(500);
+    }
+  } while(bridgeStatus === bridge.STATUSES.PENDING)
+  return bridgeStatus;
+}
+async function smartBridge(bridge) {
+  var isNotPendingStatus = await waitUntilIsNotPending(bridge);
+  if(isNotPendingStatus === bridge.STATUSES.READY) {
+    return bridge.bridge()
+      .catch(function(error) {
+        // debug('catch', error);
+        return false;
+      });
+  }
+  return false;
+}
+
 // Własny dialer oparty na klasie z dodatkowym dostępem do
 // instancji Call(i) realizowanych normalnie pod warstwa Bridge
 // (Bezpośrednie calle i ich statusy będą przydatne dla
 //  wizualizacji na froncie)
 class dialerCallWrapper {
-  public id = null;
-  public phoneNumbers = [];
-  public calls = null; // Array [0,1] of Call instances
-  public bridge = null; // Bridge (between calls) instance
 
   constructor(phoneNumber1, phoneNumber2) {
     this.id = getDialerIdByPhoneNumbers(phoneNumber1, phoneNumber2);
-    this.phoneNumbers[0] = phoneNumber1;
-    this.phoneNumbers[1] = phoneNumber2;
+    this.phoneNumbers = [phoneNumber1, phoneNumber2];
 
     this.promise = this.initializeCall();
   }
@@ -27,11 +49,14 @@ class dialerCallWrapper {
     // This way it'll only populate calls array when both calls are created
     this.calls = await Promise.all([callFactory.create(this.phoneNumbers[0]), callFactory.create(this.phoneNumbers[1])]);
     this.bridge = new Bridge(this.calls[0], this.calls[1], connector);
+    smartBridge(this.bridge);
+    return this.calls;
   }
 
   async getCallStatuses() {
     let [call1Status, call2Status] = await Promise.all([this.calls[0].getStatus(), this.calls[1].getStatus()]);
-    return {userStatus: call1Status, otherStatus: call2Status};
+    let bridgeStatus = await this.bridge.getStatus();
+    return {userStatus: call1Status, otherStatus: call2Status, bridgeStatus: bridgeStatus};
 
   }
 }
