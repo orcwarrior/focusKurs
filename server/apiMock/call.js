0e2e1b18a5211affb1bfeed5561a7a6817819b45
QuackCode <quack.code.dk@gmail.com>
2017-12-11 16:49:27 +0100

    express.routes: added call mock

---
+++ server/apiMock/call.js
@@ -0,0 +1,8 @@
+export default async function mockApiCall(req, res) {
+  return res.json({
+    status: 'SUCCESS',
+    msg: 'Udaje ze nawiazałem połączenie',
+    dialerId: null, dialer: null,
+    userNumber: 500600700
+  })
+}
--- server/routes.js
+++ server/routes.js
@@ -1,11 +1,13 @@
-import apiCall from './api/call'
+// import apiCall from './api/call'
+// import {apiStatus} from "./api/getStatus";
 import {apiStatusParamBridgeId} from './api/getStatus'
-import {mockGetStatus} from "./apiMock/getStatus";
-import {apiStatus} from "./api/getStatus";
+
+import {mockGetStatus} from './apiMock/getStatus';
+import {mockApiCall} from './apiMock/call'
 
 export default function (app) {
-  app.post('/call', apiCall);
+  app.post('/call', mockApiCall);
 
   app.param('dialerId', apiStatusParamBridgeId);
-  app.get('/status/:dialerId', apiStatus);
+  app.get('/status/:dialerId', mockGetStatus);
 }
