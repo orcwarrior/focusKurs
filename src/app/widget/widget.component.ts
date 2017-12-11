c695637457667f37c1165c52eea63887ff8e8ba3
QuackCode <quack.code.dk@gmail.com>
2017-12-11 06:44:40 +0100

    widget.component: update..

--- src/app/widget/widget.component.ts
+++ src/app/widget/widget.component.ts
@@ -2,13 +2,14 @@ import {Component, OnInit} from '@angular/core';
 import {NgForm} from '@angular/forms';
 import {Router, Routes} from '@angular/router';
 
+
 @Component({
   selector: 'app-widget',
   templateUrl: './widget.component.html',
   styleUrls: ['./widget.component.css']
 })
 export class WidgetComponent implements OnInit {
-  numer: string;
+  numer = '500127424';
   validator = /^[0-9]{9}$/;
 
   constructor(private router: Router) {
@@ -22,7 +23,7 @@ export class WidgetComponent implements OnInit {
   }
 
   onSubmit() {
+    if (!this.isValidNumber()) return;
     this.router.navigate(['/ringing/' + this.numer]);
   }
-
 }
