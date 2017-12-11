d682b0084a0e09a830a9716b142b715211fef387
QuackCode <quack.code.dk@gmail.com>
2017-12-11 17:53:06 +0100

    express.routes: mocks finally fixed

--- server/apiMock/call.js
+++ server/apiMock/call.js
@@ -2,7 +2,7 @@ export async function mockApiCall(req, res) {
   return res.json({
     status: 'SUCCESS',
     msg: 'Udaje ze nawiazałem połączenie',
-    dialerId: null, dialer: null,
-    userNumber: 500600700
+    dialerId: `mock${req.body.userNumber}`, dialer: null,
+    userNumber: req.body.userNumber
   })
 }
--- server/apiMock/getStatus.js
+++ server/apiMock/getStatus.js
@@ -1,5 +1,4 @@
-let dialersStoreMock = {};
-const STATUSES = {
+const CALLSTATUS = {
   RINGING: 'RINGING',
   CONNECTED: 'CONNECTED',
   ANSWERED: 'ANSWERED',
@@ -7,15 +6,25 @@ const STATUSES = {
   'NO ANSWER': 'NO ANSWER',
   BUSY: 'BUSY'
 }
+const BRIDGESTATUS = {
+  NEW: "NEW",
+  READY: "READY",
+  PENDING: "PENDING",
+  BRIDGED: "BRIDGED",
+  FINISHED: "FINISHED",
+  FAILED: "FAILED"
+}
+
 let statusVariantizer = 0;
+let dialersStoreMock = {};
 
 function getNextStatus(status, variant) {
-  if (status === STATUSES.RINGING)
-    if (variant % 5 === 4) return STATUSES.FAILED;
-    else return STATUSES.CONNECTED;
-  else if (status === STATUSES.CONNECTED) {
-    if (Math.random()%10 > 5) return status;
-    else if (variant < 4) return STATUSES.ANSWERED;
+  if (status === CALLSTATUS.RINGING)
+    if (variant % 5 === 4) return CALLSTATUS.FAILED;
+    else return CALLSTATUS.CONNECTED;
+  else if (status === CALLSTATUS.CONNECTED) {
+    if (Math.random() % 10 > 5) return status;
+    else if (variant < 4) return CALLSTATUS.ANSWERED;
     else {
       let statuses = ['FAILED', 'NO ANSWER', 'BUSY', 'ANSWERED'];
       let rndIdx = Math.ceil((Math.random() % 8) / 4);
@@ -25,12 +34,35 @@ function getNextStatus(status, variant) {
   else return status;
 }
 
+function updateBridge(dialMock) {
+  if (dialMock.calls > 2 && dialMock.bridgeStatus === BRIDGESTATUS.NEW) {
+    return BRIDGESTATUS.READY;
+  }
+  else if (dialMock.userStatus === CALLSTATUS.CONNECTED
+    && dialMock.otherStatus === CALLSTATUS.CONNECTED) {
+    return BRIDGESTATUS.BRIDGED;
+  }
+  else return dialMock.bridgeStatus;
+}
+
+function finishCall(dialMock) {
+  return {
+    success: false,
+    statuses: {
+      userStatus: dialMock.userStatus,
+      otherStatus: dialMock.otherStatus,
+      bridgeStatus: BRIDGESTATUS.FINISHED
+    }
+  }
+}
+
 export async function mockGetStatus(req, res) {
   let dialMock;
   if (!dialersStoreMock[req.dialerId]) {
     statusVariantizer++;
     dialMock = dialersStoreMock[req.dialerId] = {
-      userStatus: STATUSES.RINGING, otherStatus: STATUSES.RINGING,
+      userStatus: CALLSTATUS.RINGING, otherStatus: CALLSTATUS.RINGING,
+      bridgeStatus: BRIDGESTATUS.NEW,
       calls: 0, var: statusVariantizer
     };
   } else {
@@ -38,12 +70,20 @@ export async function mockGetStatus(req, res) {
 
     if (++dialMock.calls % 3 === 0) dialMock.otherStatus = getNextStatus(dialMock.otherStatus, dialMock.var);
     if (dialMock.calls % 6 === 5) dialMock.userStatus = getNextStatus(dialMock.userStatus, dialMock.var + -1);
+    dialMock.bridgeStatus = updateBridge(dialMock);
+
+    if (dialMock.calls + (Math.random() % 10) > 22) {
+      delete dialersStoreMock[req.dialerId];
+      return res.json(finishCall(dialMock));
+    }
+
   }
   return res.json({
     success: false,
     statuses: {
       userStatus: dialMock.userStatus,
-      otherStatus: dialMock.otherStatus
+      otherStatus: dialMock.otherStatus,
+      bridgeStatus: dialMock.bridgeStatus
     }
   });
 }
