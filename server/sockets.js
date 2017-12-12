0b4c032e859a85328a87ec40fe1165f7784842be
QuackCode <quack.code.dk@gmail.com>
2017-12-12 16:21:04 +0100

    express.sockets: fixed - express 4 initializes in different way a bit.

--- server/sockets.js
+++ server/sockets.js
@@ -1,15 +1,11 @@
-import http from 'http';
-import socketid from 'socket.io'
+import socketio from 'socket.io'
 
 import {call} from "./controllers/call";
 import {getStatus} from "./controllers/getStatus";
 
-export default function (expressApp) {
-  let server = http.Server(expressApp);
-  let io = socketid(server);
-
+export default function (expressServer) {
+  let io = socketio.listen(expressServer);
   io.on('connection', (socket) => {
-
     socket.on('call', async (data, callback) => {
       let callResponse = await call(data.userNumber, data.otherNumber);
       callback(callResponse); // Returns exactly same thing as the http call service
