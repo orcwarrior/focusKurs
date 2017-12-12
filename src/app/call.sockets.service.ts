a2039211ebadc8f268b151ff095dded873e43b1b
QuackCode <quack.code.dk@gmail.com>
2017-12-12 15:24:20 +0100

    call.sockets.service: created with same 'interface' that call.service uses, added socket.io-client in typings

---
+++ src/app/call.sockets.service.ts
@@ -0,0 +1,42 @@
+import {Injectable} from '@angular/core';
+import {io} from 'socket.io-client';
+import {environment} from "../environments/environment";
+
+// DK: Service should be easily to swap with call.service
+// based on HTTP Requests
+@Injectable()
+export class CallSocketsService {
+  private socketsUrl = 'http://localhost:3001/';
+  private socketCreated = false;
+  private socket; // io
+  private dialerId;
+  constructor() {
+  }
+
+  public placeCall(numer: string): Promise<any> {
+    return new Promise((resolve, reject) => {
+      if (!this.socketCreated) {
+        this._connectWithEndpoint();
+      }
+      this.socket.emit('call', {userNumber: numer, otherNumber: environment.otherNumber},
+        (response) => {
+        this.dialerId = response.dialerId;
+        resolve(response);
+        });
+    });
+  }
+
+  // NOTE: Generic way of swaping call.sockets with call.(http)
+  // is not in par with way of calling (server itself could notify
+  // about new status by socket event).
+  public checkStatus(numer: string): Promise<any> {
+    return new Promise((resolve, reject) => {
+      this.socket.emit('status', this.dialerId, (statuses) => resolve(statuses));
+    });
+  }
+
+  private _connectWithEndpoint() {
+    this.socket = io(this.socketsUrl); // Docs says socket instance wouldn't be duplicated anyway..
+    this.socketCreated = true;
+  }
+}
--- src/typings.d.ts
+++ src/typings.d.ts
@@ -3,3 +3,8 @@ declare var module: NodeModule;
 interface NodeModule {
   id: string;
 }
+declare module 'socket.io-client' {
+  var io: any;
+  var io: any;
+  export = io;
+}
