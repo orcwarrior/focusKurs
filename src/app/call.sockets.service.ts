cb5c9b33a8efe01383644b045bcba6e4ce351667
QuackCode <quack.code.dk@gmail.com>
2017-12-12 16:18:52 +0100

    call.sockets.service: socket.io-client module import fixed (with typings fixed); resolving getStatus by just statuses object.

--- src/app/call.sockets.service.ts
+++ src/app/call.sockets.service.ts
@@ -1,12 +1,12 @@
 import {Injectable} from '@angular/core';
-import {io} from 'socket.io-client';
+import * as io from 'socket.io-client';
 import {environment} from "../environments/environment";
 
 // DK: Service should be easily to swap with call.service
 // based on HTTP Requests
 @Injectable()
 export class CallSocketsService {
-  private socketsUrl = 'http://localhost:3001/';
+  private socketsUrl = 'http://localhost:3001';
   private socketCreated = false;
   private socket; // io
   private dialerId;
@@ -31,7 +31,7 @@ export class CallSocketsService {
   // about new status by socket event).
   public checkStatus(numer: string): Promise<any> {
     return new Promise((resolve, reject) => {
-      this.socket.emit('status', this.dialerId, (statuses) => resolve(statuses));
+      this.socket.emit('status', this.dialerId, (response) => resolve(response.statuses));
     });
   }
 
