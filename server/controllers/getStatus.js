8bcd627913a689bceaa5b639a2074a3e51f69e2d
QuackCode <quack.code.dk@gmail.com>
2017-12-12 15:21:56 +0100

    express: call and status api's extracted to controllers so sockets will not repeat implementation of them

---
+++ server/controllers/getStatus.js
@@ -0,0 +1,19 @@
+import {getDialer} from "../utils/dialerCall.store";
+
+const DIALER_ERR_STATUS = 'NODE: CALL NOT FOUND';
+
+export async function getStatus(dialerId) {
+  let dialer = getDialer(dialerId);
+  if (!dialer)
+    return {
+      success: false,
+      statuses: {
+        userStatus: DIALER_ERR_STATUS,
+        otherStatus: DIALER_ERR_STATUS
+      }
+    };
+  else {
+    let statuses = await dialer.getCallStatuses();
+    return {success: true, statuses: statuses};
+  }
+}
